412e3e249120af13f6ca7e77c033c5763852c6e6
v-azimniukov <v-azimniukov@microsoft.com>
2023-09-03 14:50:58 +0200

    Added System Log Repository

---
+++ EmployeeLog.DB/Repositories/SystemLogRepository.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeLog.Contracts.Enums;
+using EmployeeLog.DB.Entities;
+using EmployeeLog.Domain.Interfaces;
+
+namespace EmployeeLog.DB.Repositories;
+
+public class SystemLogRepository:ISystemLogRepository
+{
+	private readonly EmployeeDbContext _employeeDbContext;
+
+	public SystemLogRepository(EmployeeDbContext employeeDbContext) {
+		_employeeDbContext = employeeDbContext;
+	}
+
+	public async Task WriteLog(string resourceType, Event eventName, string resourceAttributes, string comment) {
+		var log = new SystemLogs()
+		{
+			ResourceType = resourceType,
+			Event = eventName.ToString(),
+			ResourceAttribute = resourceAttributes,
+			Comment = comment
+		};
+		_employeeDbContext.SystemLogs.Add(log);
+		await _employeeDbContext.SaveChangesAsync();
+	}
+}
